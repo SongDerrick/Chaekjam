95f59b66b15174d5fde2bec09281ea22c7fd6818
Derrick Song <96987446+SongDerrick@users.noreply.github.com>
2023-07-12 13:07:39 +0900

    Profile Api16

--- routes/users.js
+++ routes/users.js
@@ -31,6 +31,16 @@ router.post('/:id', autheticateToken, function(req, res, next){
     console.log('Values inserted into Contents table:', result);
   });
 
+  const sql3 = 'SELECT MAX(content_id) AS largest_content_id FROM Contents;';
+  con.query(sql3, function(err, result) {
+    if (err) {
+      console.error('Error inserting values into Reviews table:', err);
+      return res.status(500).json({ error: 'Failed to insert values into Reviews table' });
+    }
+
+    console.log('Verified from Contents table:', result);
+  });
+
   var values = [11, user_id, 1, 'Meeting', 4, start_date, start_date];
 
   const sql2 = 'INSERT INTO Reviews (meeting_id, user_id, book_id, review_type, content_id, start_date, end_date) VALUES (?, ?, ?, ?, ?, ?, ?)';
